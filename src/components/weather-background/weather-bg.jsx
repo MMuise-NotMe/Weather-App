6a482a59b0bc63bf0a26319ec3c62b2e89e61858
King Wye <84469280+MMuise-NotMe@users.noreply.github.com>
2024-06-17 20:56:10 +0800

    Made images preload when the app opens

--- src/components/weather-background/weather-bg.jsx
+++ src/components/weather-background/weather-bg.jsx
@@ -1,9 +1,21 @@
 import "./weather-bg.scss";
+import { useEffect, useMemo } from "react";
 
 export default function WeatherBackground({weatherType}) {
 
+    const pictureArray = useMemo(() => [
+        "../images/clear.jpg",
+        "../images/clouds.jpg",
+        "../images/drizzle.jpg",
+        "../images/rain.jpg",
+        "../images/snow.jpg",
+        "../images/solid-bg.PNG",
+        "../images/thunderstorm.jpg",
+        "../images/transparent.png"
+    ], []);
+
     function getDivClass() {
-        if (weatherType == "unknown") {
+        if (weatherType === "unknown") {
             return "unknown";
         } else {
             var currWeatherType = weatherType
@@ -11,6 +23,13 @@ export default function WeatherBackground({weatherType}) {
             return currWeatherType.toString().toLowerCase();
         }
     }
+
+    useEffect(() => {
+        pictureArray.forEach((picture) => {
+            const img = new Image();
+            img.src = picture.fileName;
+        });
+    }, [pictureArray]);
 
     return <div className={`full-background ${getDivClass()}`}></div>;
 }
