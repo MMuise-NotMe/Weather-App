312187dd77cf6727cecbdcc022bed4adb52359f8
King Wye <84469280+MMuise-NotMe@users.noreply.github.com>
2024-06-17 20:36:25 +0800

    Updated Favicon and Title

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import logo from "./logo.svg";
 import "./styles/App.sass";
 import Weather from "./components/weather";
+import { useEffect } from "react";
 
 function App() {
+    useEffect(() => {
+        document.title = "Weather App"
+     }, []);
     return (
         <div className="App">
             <h3 className="app-name">Weather</h3>
