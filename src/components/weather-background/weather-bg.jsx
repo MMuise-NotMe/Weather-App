26b48bc9c74175de6c99e98757ed26cd984e133b
King Wye <84469280+MMuise-NotMe@users.noreply.github.com>
2024-06-18 20:52:29 +0800

    Updated preload images and changed the body to be the one showing the images.

--- src/components/weather-background/weather-bg.jsx
+++ src/components/weather-background/weather-bg.jsx
@@ -1,35 +1,60 @@
 import "./weather-bg.scss";
-import { useEffect, useMemo } from "react";
+import {useEffect, useMemo} from "react";
 
 export default function WeatherBackground({weatherType}) {
-
-    const pictureArray = useMemo(() => [
-        "../images/clear.jpg",
-        "../images/clouds.jpg",
-        "../images/drizzle.jpg",
-        "../images/rain.jpg",
-        "../images/snow.jpg",
-        "../images/solid-bg.PNG",
-        "../images/thunderstorm.jpg",
-        "../images/transparent.png"
-    ], []);
+    const pictureArray = useMemo(
+        () => [
+            "../images/clear.jpg",
+            "../images/clouds.jpg",
+            "../images/drizzle.jpg",
+            "../images/rain.jpg",
+            "../images/snow.jpg",
+            "../images/solid-bg.PNG",
+            "../images/thunderstorm.jpg",
+            "../images/transparent.png",
+        ],
+        []
+    );
 
     function getDivClass() {
         if (weatherType === "unknown") {
             return "unknown";
         } else {
-            var currWeatherType = weatherType
-            
+            var currWeatherType = weatherType;
+
             return currWeatherType.toString().toLowerCase();
+        }
+    }
+
+    function getBodyClass() {
+        if (weatherType === "unknown") {
+            return "unknown";
+        } else {
+            return weatherType.toString().toLowerCase();
         }
     }
 
     useEffect(() => {
         pictureArray.forEach((picture) => {
             const img = new Image();
-            img.src = picture.fileName;
+            img.src = picture;
+            console.log(img.src);
         });
-    }, [pictureArray]);
+
+        const body = document.body;
+        const classList = body.classList;
+        const newClass = getBodyClass();
+
+        // Remove any previous weather classes
+        classList.remove(...classList);
+        // Add the new class
+        classList.add(newClass);
+
+        // Cleanup function to remove the class when the component unmounts
+        return () => {
+            classList.remove(newClass);
+        };
+    }, [pictureArray, weatherType]);
 
     return <div className={`full-background ${getDivClass()}`}></div>;
 }
